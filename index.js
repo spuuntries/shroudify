e8a1fb8b42c465241f004c10f8b3be7666236268
spuuntries <kek@spuun.art>
2021-12-16 18:41:33 +0700

    Make more stuff work

--- index.js
+++ index.js
@@ -7,14 +7,56 @@ const fs = require("fs"),
 function logger(msg = "logging") {
   console.log(chalk.yellow("[shroudify] ") + msg);
 }
-
-function encode(input, cipher = "kek") {
-  let cipherArr;
-  if (ciphs.includes(cipher)) {
-    fs.readFileSync(`./ciphers/${cipher}`);
+/**
+ * Encode data.
+ *
+ * @param {String|Buffer} input Data to encode.
+ * @param {Object} options Options object.
+ * @param {String} [options.cipher] Cipher to use, can be a path or a premade cipher.
+ * @param {Number} [options.rounds=1] Number of Base64 encoding rounds done.
+ * @param {String} [options.writeFile] Path to write to.
+ * @return {String|Boolean} Encoded data or if it has written to file.
+ */
+function encode(
+  input,
+  options = {
+    cipher: "kek",
+    rounds: 1,
+    writeFile: undefined,
+  }
+) {
+  let cipherArr,
+    b64a =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=".split(
+        ""
+      ),
+    /** Base64 encoded data. */
+    b64e = "";
+  if (ciphs.includes(options.cipher)) {
+    cipherArr = fs
+      .readFileSync(`./ciphers/${options.cipher}`)
+      .toString("utf8")
+      .split("\r")
+      .join("")
+      .split("\n");
+    for (let i = 1; i <= options.rounds; i++) {
+      if (i == 1) {
+        if (Buffer.isBuffer(input)) {
+          b64e = input.toString("base64");
+        } else {
+          b64e = Buffer.from(input).toString("base64");
+        }
+      } else {
+        b64e = Buffer.from(b64e).toString("base64");
+      }
+    }
+    return b64e
+      .split("")
+      .map((e) => cipherArr[b64a.indexOf(e)])
+      .join(" ");
   } else {
     try {
-      fs.readFileSync(cipher);
+      fs.readFileSync(options.cipher);
     } catch (error) {
       logger(
         chalk.red.bold(`failed to encode!
@@ -23,3 +65,5 @@ function encode(input, cipher = "kek") {
     }
   }
 }
+
+module.exports = { encode };
