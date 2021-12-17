355f86cdc3571d37368fdfcac7f251f017e23ab1
spuuntries <kek@spuun.art>
2021-12-17 07:23:31 +0700

    Custom pathing, more error handling

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logger(msg = "logging") {
  * @param {Object} options Options object.
  * @param {String} [options.cipher] Cipher to use, can be a path or a premade cipher.
  * @param {Number} [options.rounds=1] Number of Base64 encoding rounds done.
- * @param {String|Number} [options.seed] Shuffling seed.
+ * @param {any} [options.seed] Shuffling seed.
  * @param {String} [options.writeFile] Path to write to.
  * @return {String|Boolean} Encoded data or if it has written to file.
  */
@@ -58,35 +58,67 @@ function encode(
     /** Base64 encoded data. */
     b64e = "";
   if (ciphs.includes(options.cipher)) {
-    cipherArr = shuffleSeed.shuffle(
-      fs
-        .readFileSync(`./ciphers/${options.cipher}`)
-        .toString("utf8")
-        .split("\r")
-        .join("")
-        .split("\n"),
-      options.seed
-    );
-    for (let i = 1; i <= options.rounds; i++) {
-      if (i == 1) {
-        if (Buffer.isBuffer(input)) {
-          b64e = input.toString("base64");
+    try {
+      cipherArr = shuffleSeed.shuffle(
+        fs
+          .readFileSync(`./ciphers/${options.cipher}`)
+          .toString("utf8")
+          .split("\r")
+          .join("")
+          .split("\n"),
+        options.seed
+      );
+      for (let i = 1; i <= options.rounds; i++) {
+        if (i == 1) {
+          if (Buffer.isBuffer(input)) {
+            b64e = input.toString("base64");
+          } else {
+            b64e = Buffer.from(input).toString("base64");
+          }
         } else {
-          b64e = Buffer.from(input).toString("base64");
+          b64e = Buffer.from(b64e).toString("base64");
         }
-      } else {
-        b64e = Buffer.from(b64e).toString("base64");
       }
+      return b64e
+        .split("")
+        .map((e) => cipherArr[b64a.indexOf(e)])
+        .join(" ")
+        .trim()
+        .replace(/  +/g, " ");
+    } catch (error) {
+      logger(
+        chalk.red.bold(`failed to encode!
+        Err: ${error}`)
+      );
     }
-    return b64e
-      .split("")
-      .map((e) => cipherArr[b64a.indexOf(e)])
-      .join(" ")
-      .trim()
-      .replace(/  +/g, " ");
   } else {
     try {
-      fs.readFileSync(options.cipher);
+      cipherArr = shuffleSeed.shuffle(
+        fs
+          .readFileSync(options.cipher)
+          .toString("utf8")
+          .split("\r")
+          .join("")
+          .split("\n"),
+        options.seed
+      );
+      for (let i = 1; i <= options.rounds; i++) {
+        if (i == 1) {
+          if (Buffer.isBuffer(input)) {
+            b64e = input.toString("base64");
+          } else {
+            b64e = Buffer.from(input).toString("base64");
+          }
+        } else {
+          b64e = Buffer.from(b64e).toString("base64");
+        }
+      }
+      return b64e
+        .split("")
+        .map((e) => cipherArr[b64a.indexOf(e)])
+        .join(" ")
+        .trim()
+        .replace(/  +/g, " ");
     } catch (error) {
       logger(
         chalk.red.bold(`failed to encode!
